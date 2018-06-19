19f88ab97e3979cb30c7d14b451c85f2595e67df
Dharshini S <dharshis@Dharshiss-MacBook-Pro.local>
2018-06-19 13:35:08 +0530

    Implement player selection

--- src/scorer/Scorer.jsx
+++ src/scorer/Scorer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ScoreBoard from './Scoreboard';
+import PlayerSelector from "./PlayerSelector";
 
 const Scorer = () => (
-  <div>
-    <ScoreBoard />
-
-  </div>
+    <div>
+        <ScoreBoard/>
+            <PlayerSelector/>
+    </div>
 );
 
 export default Scorer;
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import gameInformationReducer from '../newGame/reducer';
+import currentballReducer from '../scorer/currentballreducer'
 
 const rootReducer = combineReducers({
   gameInformation: gameInformationReducer,
+    currentBall: currentballReducer
 });
 
 export default rootReducer;
