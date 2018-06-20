de38f7cf9e8f0815369a7aa56c6b434c5401880f
Dharshini S <dharshis@Dharshiss-MacBook-Pro.local>
2018-06-20 15:38:28 +0530

    Fixed failing tests

--- src/currentOver/reducer.test.js
+++ src/currentOver/reducer.test.js
@@ -2,7 +2,10 @@ import reducer from './reducer';
 
 describe('gameInformation/reducer', () => {
   it('should return initial state of scores of each team', () => {
-    const initialState = [0, 0, 0, 0, 0, 0];
-    expect(reducer(undefined, {})).toEqual(initialState);
+    const expectedState = {
+      ballsCount: 0,
+      currentOverScore: ['', '', '', '', '', ''],
+    };
+    expect(reducer(undefined, {})).toEqual(expectedState);
   });
 });
