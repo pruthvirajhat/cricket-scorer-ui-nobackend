cccc620b6969646fdf18cd96e99300078d3a5dd9
Dharshini S <dharshis@Dharshiss-MacBook-Pro.local>
2018-06-21 16:50:48 +0530

    overs completion check

--- src/scorer/NextBall.jsx
+++ src/scorer/NextBall.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Button } from 'reactstrap';
 import { connect } from 'react-redux';
 import { nextBallAction } from './actions';
@@ -7,23 +6,31 @@ import { nextBallAction } from './actions';
 
 const renderNextBallButton = props => (
   <div align="center">
-    <Button color="primary" onClick={() => props.onNextBallClick(props.currentBall)}>
-        Next Ball
+    <Button
+      color="primary"
+      onClick={() => props.onNextBallClick(props.currentBall)}
+      disabled={props.inningsCompleted}
+    >Next Ball
     </Button>
   </div>
 );
 
 export const mapStateToProps = state => ({
   currentBall: state.currentBall,
+  inningsCompleted:
+    (state.gameInformation.numberOfOvers === state.gameSummary.innings1.overs.length
+      && state.currentOver.ballsCount === 6),
 });
 
 export const mapDispatcherToProps = dispatch => ({
   onNextBallClick: currentBall => dispatch(nextBallAction(currentBall)),
 });
 
+
 // renderNextBallButton.propTypes = {
 //   onNextBallClick: PropTypes.func.isRequired,
 //   currentBall: PropTypes.shape.isRequired,
+//   inningsCompleted: PropTypes.bool.isRequired,
 // };
 
 export default connect(mapStateToProps, mapDispatcherToProps)(renderNextBallButton);
